254ffdb4a0918f8c4222be625433a1dfc523ba5a
TFK <tfk@example.com>
2026-05-03 00:04:06 +0530

    add full firebase credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,18 +1,17 @@
 // Firebase Configuration
-// Replace the config object with your own Firebase project credentials
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    databaseURL: "https://app-store-1959f-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDdzc-_i3K_XYjP9r_qFZxH-4E6iECeNys",
+  authDomain: "app-store-1959f.firebaseapp.com",
+  databaseURL: "https://app-store-1959f-default-rtdb.firebaseio.com",
+  projectId: "app-store-1959f",
+  storageBucket: "app-store-1959f.firebasestorage.app",
+  messagingSenderId: "1024099820645",
+  appId: "1:1024099820645:web:52b9bec7cf7937e98a128c"
 };
 
 // Initialize Firebase
